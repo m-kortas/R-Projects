af3aad6acb6c79f7e4afe0d86fb8436b9d67de60
Magdalena Kortas <magdalenekortas@gmail.com>
2018-03-24 12:54:42 +0100

    text mining

--- textmining/1.R
+++ textmining/1.R
@@ -6,3 +6,9 @@ library(tm)
 library(SnowballC)
 library(wordcloud)
 library(RColorBrewer)
+
+filePath <- "http://www.gutenberg.org/cache/epub/103/pg103.txt"
+text <- readLines(filePath)
+text
+
+docs <- Corpus(VectorSource(test))
