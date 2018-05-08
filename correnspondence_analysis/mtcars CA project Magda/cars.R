ed5bf53c3034ab57d7e54520d3920814023b1633
Magdalena Kortas <magdalenekortas@gmail.com>
2018-05-08 21:32:00 +0200

    deletion

--- correnspondence_analysis/mtcars CA project Magda/cars.R
+++ correnspondence_analysis/mtcars CA project Magda/cars.R
@@ -28,8 +28,11 @@ mtcars$carb <- normalize(mtcars$carb)
 
 mtcars
 
-dt <- as.table(as.matrix(mtcars))   # to table 
+dt <- as.table(as.matrix(mtcars))   # all to table
+dt
 
+
+###
 
 balloonplot(t(dt), main = "mtcars", xlab="", ylab="", 
             label = TRUE, show.margins = FALSE)  #graph of contingency table
@@ -50,3 +53,27 @@ fviz_contrib(ca_result, choice = "row", axes = 2, top = 10) #contribution to  di
 
 fviz_ca_biplot(ca_result, col.row = "contrib", repel = TRUE) #symmetric plot
 #Points with similar profile are closer on the factor map
+
+### for 3 pairs only
+
+# Making CA for 32 row categories and 3 column variables: Error: Each group consists of only one observation. Not enough data to make biplot for CA
+# Making CA for 3 row categories and 3 column variables (3 pairs): not enough data to draw interesting conclusions
+# Making CA for 32 row categories and 2 column variable (1 pair): Each group consists of only one observation. Not enough data to make biplot for CA
+  
+dt1 <- as.table(as.matrix(mtcars[c(1:32),c(1:3)])) 
+dt1
+dt2 <- as.table(as.matrix(mtcars[c(1:3),c(1:3)])) 
+dt2
+dt3 <- as.table(as.matrix(mtcars[c(1:32),c(1:2)])) 
+dt3
+
+balloonplot(t(dt1), main = "mtcars", xlab="", ylab="",  label = TRUE, show.margins = FALSE) 
+ca_result1 <- CA(dt1, graph =FALSE)
+print(ca_result1)    
+eig.val1 <- get_eigenvalue(ca_result1) 
+eig.val1 
+fviz_screeplot(ca_result1, addlabels = TRUE, ylim = c(0,50))
+fviz_contrib(ca_result1, choice = "row", axes = 1, top = 10) 
+fviz_contrib(ca_result1, choice = "row", axes = 2, top = 10) 
+fviz_ca_biplot(ca_result1, col.row = "contrib", repel = TRUE)
+
